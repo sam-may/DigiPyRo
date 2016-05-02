61053d266e9ca59e8b852f207588ef9e295ba3f3
Sam May <sammay@ucla.edu>
2016-05-02 13:36:08 -0700

    Various changes.

--- DigiPyRo.py
+++ DigiPyRo.py
@@ -63,9 +63,35 @@ def circumferencePoints(event, x, y, flags, param):
             cv2.circle(frame, center, r, (0,255,0), 1)
         cv2.imshow('CenterClick', frame) 
         frame = clone.copy()
+
+def nGon(event, x, y, flags, param):
+    global npts, center, frame, xpoints, ypoints, r, poly1, poly2
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if (npts == 0):
+            xpoints = np.array([x])
+            ypoints = np.array([y])
+        else:
+            xpoints = np.append(xpoints,x)
+            ypoints = np.append(ypoints,y)
+        npts+=1
+        cv2.circle(frame, (x,y), 3, (0,255,0), -1)
+        clone = frame.copy()
+        if (len(xpoints) > 2):
+            center = (int(np.sum(xpoints)/npts), int(np.sum(ypoints)/npts))
+            poly1 = np.array([[0,0],[frame.shape[1],0],[frame.shape[1],frame.shape[0]], [0,frame.shape[0]]])
+            poly2 = np.array([[xpoints[0],ypoints[0]]])
+            for i in range(len(xpoints)-1):
+                nextpt = np.array([[xpoints[i+1], ypoints[i+1]]])
+                poly2 = np.append(poly2,nextpt,axis=0)
+                cv2.line(frame, (xpoints[i], ypoints[i]), (xpoints[i+1], ypoints[i+1]), (0, 255, 0), 1)
+            cv2.line(frame, (xpoints[len(xpoints)-1], ypoints[len(xpoints)-1]), (xpoints[0],ypoints[0]), (0, 255, 0), 1)       
+            cv2.circle(frame, center, 4, (255,0,0), -1)
+        cv2.imshow('CenterClick', frame) 
+        frame = clone.copy()
+
  
 def removePoint(orig):
-    global npts, center, frame, xpoints, ypoints, r, poly1, poly2
+    global npts, center, frame, xpoints, ypoints, r, poly1, poly2, custMask
     if npts == 0:
         return
 
@@ -85,19 +111,29 @@ def removePoint(orig):
     for i in range(len(xpoints)):
         cv2.circle(frame, (xpoints[i], ypoints[i]), 3, (0,255,0), -1)
     if (len(xpoints) > 2):
-        bestfit = calc_center(xpoints, ypoints)
-        center = (bestfit[0], bestfit[1])
-        r = bestfit[2]
-        poly1 = np.array([[0,0],[frame.shape[1],0],[frame.shape[1],frame.shape[0]], [0,frame.shape[0]]])
-        poly2 = np.array([[bestfit[0]+r,bestfit[1]]])
-        circpts = 100
-        for i in range(1,circpts):
-            theta =  2*np.pi*(float(i)/circpts)
-            nextpt = np.array([[int(bestfit[0]+(r*np.cos(theta))),int(bestfit[1]+(r*np.sin(theta)))]])
-            poly2 = np.append(poly2,nextpt,axis=0)
-        cv2.circle(frame, center, 4, (255,0,0), -1)
-        cv2.circle(frame, center, r, (0,255,0), 1)
-    cv2.imshow('CenterClick', frame)
+        if custMask:
+            poly1 = np.array([[0,0],[frame.shape[1],0],[frame.shape[1],frame.shape[0]], [0,frame.shape[0]]])
+            poly2 = np.array([[xpoints[0],ypoints[0]]])
+            for i in range(len(xpoints)-1):
+                nextpt = np.array([[xpoints[i+1], ypoints[i+1]]])
+                poly2 = np.append(poly2,nextpt,axis=0)
+                cv2.line(frame, (xpoints[i], ypoints[i]), (xpoints[i+1], ypoints[i+1]), (0, 255, 0), 1)
+            cv2.line(frame, (xpoints[len(xpoints)-1], ypoints[len(xpoints)-1]), (xpoints[0],ypoints[0]), (0, 255, 0), 1)
+            cv2.circle(frame, center, 4, (255,0,0), -1)
+        else:
+            bestfit = calc_center(xpoints, ypoints)
+            center = (bestfit[0], bestfit[1])
+            r = bestfit[2]
+            poly1 = np.array([[0,0],[frame.shape[1],0],[frame.shape[1],frame.shape[0]], [0,frame.shape[0]]])
+            poly2 = np.array([[bestfit[0]+r,bestfit[1]]])
+            circpts = 100
+            for i in range(1,circpts):
+                theta =  2*np.pi*(float(i)/circpts)
+                nextpt = np.array([[int(bestfit[0]+(r*np.cos(theta))),int(bestfit[1]+(r*np.sin(theta)))]])
+                poly2 = np.append(poly2,nextpt,axis=0)
+            cv2.circle(frame, center, 4, (255,0,0), -1)
+            cv2.circle(frame, center, r, (0,255,0), 1)
+        cv2.imshow('CenterClick', frame)
 
 def calc_center(xp, yp):
     n = len(xp)
@@ -226,29 +262,45 @@ def calcDeriv(f, t):
     df[1:len(f)-1] = f[0:len(f)-2]*((t[1:len(f)-1] - t[2:len(f)]) / ((t[0:len(f)-2] - t[1:len(f)-1])*(t[0:len(f)-2] - t[2:len(f)]))) + f[1:len(f)-1]*(((2*t[1:len(f)-1]) - t[0:len(f)-2] - t[2:len(f)]) / ((t[1:len(f)-1] - t[0:len(f)-2])*(t[1:len(f)-1] - t[2:len(f)]))) + f[2:len(f)]*((t[1:len(f)-1] - t[0:len(f)-2]) / ((t[2:len(f)] - t[0:len(f)-2])*(t[2:len(f)] - t[1:len(f)-1]))) 
     return df
 
+def splineFit(x, y, deg):
+    fit = np.polyfit(x, y, deg)
+    yfit = np.zeros(len(y))
+    for i in range(deg):
+        yfit += fit[i]*(x**(deg-(i+1)))
+    return yfit
+
 def start():
     vid = cv2.VideoCapture(filenameVar.get())
 
     global width, height, numFrames, fps, fourcc, video_writer, spinlab, npts
     npts = 0
     spinlab = cv2.imread('/Users/sammay/Desktop/SPINLab/DigiRo/spinlogo.png')
-    width = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-    height = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+    #width = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+    #height = int(vid.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+    width = 1280
+    height = 720
     fps = fpsVar.get()
     fileName = savefileVar.get()
     fourcc = cv2.cv.CV_FOURCC('m','p','4','v')
     video_writer = cv2.VideoWriter(fileName+'.avi', fourcc, fps, (width, height))
 
-    global physicalRPM, digiRPM, dtheta, per
+    global naturalRPM, physicalRPM, digiRPM, dtheta, per, custMask
+    naturalRPM = tableRPMVar.get()
+    naturalOmega = (naturalRPM * 2*np.pi)/60
     physicalRPM = physRPMVar.get()
     digiRPM = digiRPMVar.get()
+    curvRPM = naturalRPM - np.abs(physicalRPM)
     totRPM = physicalRPM + digiRPM
     totOmega = (totRPM *2*np.pi)/60
     dtheta = digiRPM*(6/fps)
     per = 60*(1 / np.abs(float(digiRPM)))
+    addRot = digiRPM != 0
 
     startFrame = fps*startTimeVar.get()
+    if startFrame == 0:
+        startFrame = 1
     numFrames = int(fps*(endTimeVar.get() - startTimeVar.get()))
+    custMask = customMaskVar.get()
     trackBall = trackVar.get()
     makePlots = plotVar.get()
 
@@ -260,8 +312,12 @@ def start():
     # Open first frame from video, user will click on center
     vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, startFrame)
     ret, frame = vid.read()
+    frame = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
     cv2.namedWindow('CenterClick')
-    cv2.setMouseCallback('CenterClick', circumferencePoints)
+    if custMask:
+        cv2.setMouseCallback('CenterClick', nGon)
+    else:
+        cv2.setMouseCallback('CenterClick', circumferencePoints)
 
     instructsCenter(frame)
     orig = frame.copy()
@@ -279,6 +335,7 @@ def start():
     if trackBall:
         vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, startFrame)
         ret, frame = vid.read()
+        frame = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
         cv2.namedWindow('Locate Ball')
         cv2.setMouseCallback('Locate Ball', locate)
 
@@ -291,27 +348,34 @@ def start():
     t = np.empty(numFrames)
     ballX = np.empty(numFrames)
     ballY = np.empty(numFrames)
-    ballPts = 0 #will identify the number of times that Hough Circle transform identifies the ball
-    lastLoc = particleCenter
-    thresh = 50
-    trackingData = np.zeros(numFrames) # logical vector which tells us if the ball was tracked at each frame
+    if trackBall:
+        ballPts = 0 #will identify the number of times that Hough Circle transform identifies the ball
+        lastLoc = particleCenter
+        thresh = 150
+        trackingData = np.zeros(numFrames) # logical vector which tells us if the ball was tracked at each frame
     framesArray = np.empty((numFrames,height,width,3), np.uint8)
     for i in range(numFrames):
         ret, frame = vid.read() # read next frame from video
+        frame = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
+        cv2.fillPoly(frame, np.array([poly1, poly2]), 0)
+        cv2.circle(frame, center, 4, (255,0,0), -1)
+        #frame = centerImg(frame, center[0], center[1])
 
-        M = cv2.getRotationMatrix2D(center, i*dtheta, 1.0)
-        rotated = cv2.warpAffine(frame, M, (width, height))
-        cv2.fillPoly(rotated, np.array([poly1, poly2]), 0)
-        cv2.circle(rotated, center, 4, (255,0,0), -1)
-        centered = centerImg(rotated, center[0], center[1])
+        if addRot:
+            M = cv2.getRotationMatrix2D(center, i*dtheta, 1.0)
+            frame = cv2.warpAffine(frame, M, (width, height))
+        
+        #cv2.fillPoly(frame, np.array([poly1, poly2]), 0)
+        #cv2.circle(frame, center, 4, (255,0,0), -1)
+        frame = centerImg(frame, center[0], center[1])
     
     
-        centered = cv2.resize(centered,(width,height), interpolation = cv2.INTER_CUBIC)
+        centered = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
         
         if trackBall:
             gray = cv2.cvtColor(centered, cv2.COLOR_BGR2GRAY)
             gray = cv2.medianBlur(gray,5)
-            ballLoc = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=15, minRadius = int(particleRadius * 0.7), maxRadius = int(particleRadius * 1.3))
+            ballLoc = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=10, minRadius = int(particleRadius * 0.6), maxRadius = int(particleRadius * 1.4))
             if type(ballLoc) != NoneType :
                 for j in ballLoc[0,:]:
                     if (np.abs(j[0] - lastLoc[0]) < thresh) and (np.abs(j[1] - lastLoc[1]) < thresh):    
@@ -347,7 +411,7 @@ def start():
                 ballTheta[i] += 2*np.pi
 
         omega = (np.pi)/3
-        dataFitPolar = fitDataPolar(np.array([t, ballR, ballTheta, digiRPM + physicalRPM]), np.array([ballR[0],0.0,0,totOmega,totOmega]))
+        dataFitPolar = fitDataPolar(np.array([t, ballR, ballTheta, digiRPM + physicalRPM]), np.array([ballR[0],0.0,0,naturalOmega,totOmega]))
         modelR = createModelR(dataFitPolar, t)
         modelTheta = createModelTheta(t, dataFitPolar, ballTheta[0])
 
@@ -355,12 +419,13 @@ def start():
         fExp = 2*dataFitPolar[3]
         fErr = np.abs((fTh - fExp) / fTh)
 
-        diagnostic1 = 'Best estimate of: \n'
-        diagnostic2 = '    Inertial frequency, f: ' + str(2*dataFitPolar[3]) + '\n'
-        diagnostic3 = '    '+ str(fErr*100) + '% error from theoretical value based on input RPM \n'
-        diagnostic4 = '    Damping coefficient, d: ' + str(dataFitPolar[1]) + '\n'
-        diag = diagnostic1 + diagnostic2 + diagnostic3 + diagnostic4
-        print diag
+        if totOmega != 0:
+            diagnostic1 = 'Best estimate of: \n'
+            diagnostic2 = '    Inertial frequency, f: ' + str(2*dataFitPolar[3]) + '\n'
+            diagnostic3 = '    '+ str(fErr*100) + '% error from theoretical value based on input RPM \n'
+            diagnostic4 = '    Damping coefficient, d: ' + str(dataFitPolar[1]) + '\n'
+            diag = diagnostic1 + diagnostic2 + diagnostic3 + diagnostic4
+            print diag
 
         if makePlots:
             plt.figure(1)
@@ -415,6 +480,7 @@ def start():
         dataFile.close()
 
         rInert = utot / fExp # inertial radius
+        rInertSmooth = splineFit(t, rInert, 10)
 
         if makePlots:
             plt.figure(3)
@@ -425,6 +491,7 @@ def start():
             plt.legend(loc = 'upper right', fontsize = 'x-small')
             plt.subplot(222)
             plt.plot(t, rInert)
+            plt.plot(t, rInertSmooth)
             plt.xlabel(r"$t$ (s)")
             plt.ylabel(r"$r_i$")
             plt.subplot(223)
@@ -455,56 +522,71 @@ def start():
                 cv2.line(frame, lineStart, lineEnd, (255, 255, 255), 1)
                 index+=1
             video_writer.write(frame)
+    else:
+        for i in range(numFrames):
+            frame = framesArray[i]
+            video_writer.write(frame)
     
     video_writer.release()
 
 root = Tk()
 root.title('DigiPyRo')
 startButton = Button(root, text = "Start!", command = start)
-startButton.grid(row=6, column=0)
+startButton.grid(row=8, column=0)
+
+tableRPMVar = DoubleVar()
+tableRPMEntry = Entry(root, textvariable=tableRPMVar)
+tableLabel = Label(root, text="Curvature of table (in RPM, enter 0 for a flat surface):")
+tableRPMEntry.grid(row=0, column=1)
+tableLabel.grid(row=0, column=0)
+
 digiRPMVar = DoubleVar()
 physRPMVar = DoubleVar()
 digiRPMEntry = Entry(root, textvariable=digiRPMVar)
 physRPMEntry = Entry(root, textvariable=physRPMVar)
 digiLabel = Label(root, text="Additional digital rotation (RPM):")
 physLabel = Label(root, text="Physical rotation (RPM):")
-digiRPMEntry.grid(row=1, column=1)
-physRPMEntry.grid(row=0, column=1)
-digiLabel.grid(row=1, column=0)
-physLabel.grid(row=0, column=0)
+digiRPMEntry.grid(row=2, column=1)
+physRPMEntry.grid(row=1, column=1)
+digiLabel.grid(row=2, column=0)
+physLabel.grid(row=1, column=0)
+
+customMaskVar = BooleanVar()
+customMaskEntry = Checkbutton(root, text="Custom-Shaped Mask (checking this box allows for a polygon-shaped mask. default is circular)", variable=customMaskVar)
+customMaskEntry.grid(row=3, column=0)
 
 filenameVar = StringVar()
 filenameEntry = Entry(root, textvariable = filenameVar)
 filenameLabel = Label(root, text="Full filepath to movie:")
-filenameEntry.grid(row=2, column=1)
-filenameLabel.grid(row=2, column=0)
+filenameEntry.grid(row=4, column=1)
+filenameLabel.grid(row=4, column=0)
 
 savefileVar = StringVar()
 savefileEntry = Entry(root, textvariable = savefileVar)
 savefileLabel = Label(root, text="Save output video as:")
-savefileEntry.grid(row=3, column=1)
-savefileLabel.grid(row=3, column=0)
+savefileEntry.grid(row=5, column=1)
+savefileLabel.grid(row=5, column=0)
 
 startTimeVar = DoubleVar()
 endTimeVar = DoubleVar()
 startTimeEntry = Entry(root, textvariable = startTimeVar)
 endTimeEntry = Entry(root, textvariable = endTimeVar)
 startTimeLabel = Label(root, text="Start and end times (in seconds):")
-startTimeLabel.grid(row=4, column=0)
-startTimeEntry.grid(row=4, column=1)
-endTimeEntry.grid(row=4, column=2)
+startTimeLabel.grid(row=6, column=0)
+startTimeEntry.grid(row=6, column=1)
+endTimeEntry.grid(row=6, column=2)
 
 trackVar = BooleanVar()
 trackEntry = Checkbutton(root, text="Track Ball", variable=trackVar)
-trackEntry.grid(row=3, column=2)
+trackEntry.grid(row=5, column=2)
 plotVar = BooleanVar()
 plotEntry = Checkbutton(root, text="Create plots with tracking results", variable=plotVar)
-plotEntry.grid(row=3, column=3)
+plotEntry.grid(row=5, column=3)
 
 fpsVar = DoubleVar()
 fpsEntry = Entry(root, textvariable=fpsVar)
 fpsLabel = Label(root, text="Frames per second of video:")
-fpsEntry.grid(row=5, column=1)
-fpsLabel.grid(row=5, column=0)
+fpsEntry.grid(row=7, column=1)
+fpsLabel.grid(row=7, column=0)
 
 root.mainloop()
