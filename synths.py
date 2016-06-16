57d4cc7dc92be0f670e856052490353201394e51
Sam May <sammay@ucla.edu>
2016-06-16 11:52:43 -0700

    Working on #6

--- synths.py
+++ synths.py
@@ -1,3 +1,8 @@
+# This program creates a synthetic .avi movie for use with DigiPyRo
+# The video shows a ball rolling on a parabolic surface
+# The user may change the length of the movie[1], the frame rate of the movie[2], the frequency of oscillations[3], the size of the ball[4], and choose to add/remove frictional effects[5]
+
+# Import necessary modules
 import cv2
 import numpy as np
 from Tkinter import *
@@ -7,53 +12,76 @@ from matplotlib import pyplot as plt
 import scipy as sp
 from scipy.optimize import leastsq
 
-fps = 30.0
-width = 2*1260
-height = 2*720
+# Ask user for movie name
+saveFile = raw_input('Enter a name for the movie (e.g. mySyntheticMovie): ')
+saveFile += '.avi'
+
+# Define movie details
+movLength = 10				# [1] define the desired length of the movie in seconds
+fps = 30.0      			# [2] Set this to a low value (10-15) for increased speed or a higher value (30-60) for better results with DigiPyRo
+width = 1260				# Width and height in pixels
+height = 720    			# Decrease the width and height for increased speed, increase for improved resolution
+
+# Define ball and table values
+ballSize = 24				# [4] radius of ball in pixels. Good values might be 15-30
+rpm = 10				# [3] frequency of oscillations (in RPM). Good values might be 5-15
+friction = False			# [5] set to "False" for no friction (harmonic oscillator) or "True" to add friction (damped harmonic oscillator)
+dampCoeff = 0.1				# coefficient of friction (applicable only if "friction = True"). Good values might be 0.1-0.2
+
+
+# Set the amplitude of oscillations to 40% of the smaller dimension
+amp = 0
+if width > height:
+    amp = int(0.4*height)
+else:
+    amp = int(0.4*width)  
+ 				
+initPos = (width/2, height/2 - amp) 	# initial xy-coordinates of ball
+omega = (rpm * 2 * np.pi)/60		# calculate angular frequency of oscillations 
+
+
+
+# Create movie file
 fourcc = cv2.cv.CV_FOURCC('m','p','4','v')
-saveFile = '/Users/sammay/Desktop/SPINLab/DigiRo/syntheticMovie.avi'
 video_writer = cv2.VideoWriter(saveFile, fourcc, fps, (width, height))
 
-ballSize = 24
-yoff = -400
-initPos = (width/2, height/2 + yoff)
-rpm = 10
-omega = (rpm * 2 * np.pi)/60
-
 def harmOsc(t):
-    return (width/2, int(height/2 + (yoff*np.cos(omega*t)) + 0.5))
+    return (width/2, int(height/2 + (amp*np.cos(omega*t)) + 0.5))
 
-dampCoeff = 0.1
-wd = omega*((1-(dampCoeff**2))**(0.5))
+wd = omega*((1-(dampCoeff**2))**(0.5))	# calculate frequency of damped oscillations
 
 def dampOsc(t):
-    return (width/2, int(height/2 + (yoff*np.exp(-dampCoeff*omega*t)*np.cos(wd*t)) + 0.5))
+    return (width/2, int(height/2 + (amp*np.exp(-dampCoeff*omega*t)*np.cos(wd*t)) + 0.5))
 
 def updatePos(oscillatorType, t):
     return oscillatorType(t)
 
-numFrames = 300
+numFrames = int(movLength * fps)	# calculate number of frames in movie
+
+oscType = 0
+if friction:
+    oscType = dampOsc
+else:
+    oscType = harmOsc
+
 
 for i in range(numFrames):
-    frame = np.zeros((height,width, 3), np.uint8)
-    #frame = np.random.randint(low=0, high=256, size=(height,width, 3)).astype(np.uint8)
-    #frame = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
+    frame = np.zeros((height,width,3), np.uint8)
 
     # Outline of circular table
-    cv2.circle(frame,(width/2, height/2), 500, (255,255,255), 2) 
+    cv2.circle(frame,(width/2, height/2), int(1.1*amp), (255,255,255), 2) 
     
     # Place marker at center of table
-    ls = 5 # line length (in pixels)
+    ls = 5 				# line length (in pixels)
     cv2.line(frame, (width/2+ls, height/2), (width/2-ls, height/2), (255,255,255), 1)
     cv2.line(frame, (width/2, height/2+ls), (width/2, height/2-ls), (255,255,255), 1)
 
     # Calculate new position of ball and draw it
-    currentPos = updatePos(harmOsc, i/fps)
+    currentPos = updatePos(oscType, i/fps)
     cv2.circle(frame, currentPos, ballSize, (255,255,255), -1)
     M = cv2.getRotationMatrix2D((width/2, height/2), 45, 1.0)
     frame = cv2.warpAffine(frame, M, (width, height))
     frame = cv2.resize(frame,(width,height), interpolation = cv2.INTER_CUBIC)
     video_writer.write(frame)
-    print i
 
 video_writer.release()
