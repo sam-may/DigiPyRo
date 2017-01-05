ff465f95abea623daebda4f5b297bde289776671
Sam May <sammay@ucla.edu>
2017-01-05 10:14:38 -0800

    Working on #11: create setup file for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'DigiPyRo',
+  packages = ['DigiPyRo'],
+  version = '2.1',
+  description = 'Digital Rotation and Single Particle Tracking Software',
+  author = 'Samuel May',
+  author_email = 'sjmay@ucsd.edu',
+  url = 'https://github.com/sam-may/DigiPyRo',
+  download_url = 'https://github.com/sam-may/DigiPyRo/tarball/0.1',
+  keywords = [],
+  classifiers = [],
+)
